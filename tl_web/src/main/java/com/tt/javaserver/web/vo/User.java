324f5e6a2666e68e9d51dcb5fa89d704cb6ee89e
GCL <gcochenglong@163.com>
2017-05-13 10:47:18 +0800

    test Github

--- tl_web/src/main/java/com/tt/javaserver/web/vo/User.java
+++ tl_web/src/main/java/com/tt/javaserver/web/vo/User.java
@@ -28,6 +28,25 @@ public class User implements Serializable {
 
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        return userName != null ? userName.equals(user.userName) : user.userName == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
